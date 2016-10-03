62ad12c9aaf2c45ebb99327df7f5ba872f74f521
bradmarshall <echoing.waves.of.sound@gmail.com>
2016-10-02 20:57:36 -0400

    Added ability to upload files! Uploaded SVG files are currently automatically sized to 500x500... hoping to set size based on width/height attributes and/or viewbox soon.

--- js/main.js
+++ js/main.js
@@ -1,45 +1,45 @@
 
 document.addEventListener("DOMContentLoaded", function() {
-  var svg = document.querySelector("svg");
+  //var svg = document.querySelector("svg");
   var toolPanel = document.querySelector("aside.tool-panel");
   var workSpace = document.querySelector("main.work-space");
 
   var graphic = null;
+  var parsedSVG = null;
 
   var artboardIsVisible = false;
-
-  centerSVG(svg, toolPanel); // Start with a centered SVG.
-  load("blinky");
 
   var svgCurrentPosX = 0;
   var svgCurrentPosY = 0;
 
   document.addEventListener("mousedown", function(e) {
-    var mouseStartX = e.clientX;
-    var mouseStartY = e.clientY;
+    if(graphic) {
+      var mouseStartX = e.clientX;
+      var mouseStartY = e.clientY;
 
-    var svgStartPosX = svg.getBoundingClientRect().left;
-    var svgStartPosY = svg.getBoundingClientRect().top;
+      var svgStartPosX = graphic.getBoundingClientRect().left;
+      var svgStartPosY = graphic.getBoundingClientRect().top;
 
-    window.evtMouseMove = function(e) {
-      var mouseNewX = e.clientX;
-      var mouseNewY = e.clientY;
+      window.evtMouseMove = function(e) {
+        var mouseNewX = e.clientX;
+        var mouseNewY = e.clientY;
 
-      var dragDistanceX = (mouseNewX - mouseStartX);
-      var dragDistanceY = (mouseNewY - mouseStartY);
+        var dragDistanceX = (mouseNewX - mouseStartX);
+        var dragDistanceY = (mouseNewY - mouseStartY);
 
-      svgCurrentPosX = (svgStartPosX + dragDistanceX) - workSpace.offsetLeft;
-      svgCurrentPosY = (svgStartPosY + dragDistanceY) - workSpace.offsetTop;
+        svgCurrentPosX = (svgStartPosX + dragDistanceX) - workSpace.offsetLeft;
+        svgCurrentPosY = (svgStartPosY + dragDistanceY) - workSpace.offsetTop;
 
-      // Set new position relative to top left of WORKSPACE.
-      svg.style.left = svgCurrentPosX + "px";
-      svg.style.top  = svgCurrentPosY + "px";
-    };
+        // Set new position relative to top left of WORKSPACE.
+        graphic.style.left = svgCurrentPosX + "px";
+        graphic.style.top  = svgCurrentPosY + "px";
+      };
 
-    // If started with a left-click, begin tracking mouse movements.
-    if(e.button == 0) {
-      document.addEventListener("mousemove", window.evtMouseMove);
-      workSpace.classList.add("grabbing");
+      // If started with a left-click, begin tracking mouse movements.
+      if(e.button == 0) {
+        document.addEventListener("mousemove", window.evtMouseMove);
+        workSpace.classList.add("grabbing");
+      }
     }
   });
 
@@ -54,6 +54,7 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 
 
+  // centerSVG():
   // toolPanelElem is optional. Include it if you want this function
   // to take into account the presence of a tool panel and offset
   // it's final center value. Produces "optical" centering.
@@ -82,8 +83,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
   // Load a new SVG document.
   function load(id) {
-    graphic = SVG(id);
-    console.log(graphic);    
+    // TODO: jam the svg-loading in here when we refactor this mess.
   }
 
   function processFilters() {
@@ -94,29 +94,71 @@ document.addEventListener("DOMContentLoaded", function() {
     artboardIsVisible = !artboardIsVisible;
     
     if(artboardIsVisible) {
-      graphic.addClass("artboardIsVisible");
+      workSpace.classList.add("artboardIsVisible");
       btnToggleArtboard.textContent = "Hide";
     } else {
-      graphic.removeClass("artboardIsVisible");
+      workSpace.classList.remove("artboardIsVisible");
       btnToggleArtboard.textContent = "Show";
     }
   }
 
   // ------------------------------------------ [ ToolPanel Controls ]
 
-  var btnCenterSVG = document.querySelector("#centerSVG");
+  var btnCenterSVG = document.querySelector("#center-svg");
   btnCenterSVG.addEventListener("click", function() {
-    centerSVG(svg, toolPanel);
+    centerSVG(graphic, toolPanel);
   });
 
 
-  var btnProcessFilters = document.querySelector("#processFilters");
+  var btnProcessFilters = document.querySelector("#process-filters");
   btnProcessFilters.addEventListener("click", function() {
     processFilters();
   });
 
-  var btnToggleArtboard = document.querySelector("#toggleArtboard");
+  var btnToggleArtboard = document.querySelector("#toggle-artboard");
   btnToggleArtboard.addEventListener("click", function() {
     toggleArtboard();
   });
+
+
+  var fileUploadControl = document.querySelector("#file-upload-control");
+  var inputFileUpload   = document.querySelector("#file-input");
+
+  if(inputFileUpload) {
+    inputFileUpload.addEventListener("change", function(e) {
+      if(this.files.length) {
+        var file = this.files[0];
+
+        if(file.type == "image/svg+xml") {
+          var fileReader = new FileReader();
+
+          fileReader.addEventListener("load", function() {
+            workSpace.innerHTML = this.result;
+            graphic = document.querySelector(".work-space > svg");
+
+            parsedSVG = SVG(graphic);
+            parsedSVG.size(500, 500);
+
+            centerSVG(graphic, toolPanel);
+          });
+
+          fileReader.addEventListener("error", function() {
+            alert("Sorry, file could not be uploaded for unknown reasons.");
+          });
+
+          fileReader.readAsText(file);
+        } else {
+          alert("File type must be SVG! Please check your file or try a different one.");
+        }
+      }
+    });
+  }
+
+  if(fileUploadControl) {
+    fileUploadControl.addEventListener("click", function() {
+      if(inputFileUpload) {
+        inputFileUpload.click();
+      }
+    });
+  }
 });
